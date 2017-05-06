f9c82d9df325a7a5597cfd44f6e621f43c53019a
Kevin Dam <kevinddam@gmail.com>
2017-05-05 21:22:11 -0400

    add modal for pop-up-menu

--- lib/menu_component.jsx
+++ lib/menu_component.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import Modal from 'react-modal';
+
 
 class Menu extends React.Component {
   constructor(props){
     super(props);
+    const appElement = document.getElementById('canvas');
+    Modal.setAppElement(appElement);
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
     this.handleStart = this.handleStart.bind(this);
-    this.state = { buttonText: "START"};
+    this.state = { buttonText: "START", modalIsOpen: true};
+  }
+
+  openModal(){
+    this.setState({modalIsOpen: true});
+  }
+
+  closeModal(){
+    this.setState({modalIsOpen: false});
   }
 
   handleStart(){
+    this.closeModal();
     this.props.ssGame.togglePause();
     this.props.ssGame.run();
     const buttonText = this.state.buttonText === "START" ? "PAUSE" : "START";
@@ -16,13 +31,22 @@ class Menu extends React.Component {
 
   render(){
     return(
-      <div>
-        <p >
-          Your name is Lyn Pham, a young fledgling wizard. Your twin sister
-          Lily has been taken hostage by Lord Voldemort! Reach her while avoiding
-          Voldemort's slew of Avada Kedavras to get your sister back!
-        </p>
-        <button onClick={ this.handleStart }>{this.state.buttonText}</button>
+      <div className="nav-menu-parent">
+        <button className="nav-button" onClick={ this.openModal }>MENU</button>
+        <Modal
+          className="nav-menu"
+          overlayClassName='nav-menu-overlay'
+          isOpen={this.state.modalIsOpen}
+          onRequestClose={this.closeModal}
+          contentLabel="Modal"
+        >
+          <p >
+            Your name is Lyn Pham, a young fledgling wizard. Your twin sister
+            Lily has been taken hostage by Lord Voldemort! Reach her while avoiding
+            Voldemort's slew of Avada Kedavras to get your sister back!
+          </p>
+          <button onClick={ this.handleStart }>{this.state.buttonText}</button>
+        </Modal>
       </div>
     );
   }
