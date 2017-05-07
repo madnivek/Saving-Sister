dcc07fc7cb7004431da606e3d8c8c7071b514672
Kevin Dam <kevinddam@gmail.com>
2017-05-07 01:48:54 -0400

    resized canvas and assets

--- lib/main.jsx
+++ lib/main.jsx
@@ -18,14 +18,15 @@ document.addEventListener('DOMContentLoaded', () => {
     './assets/platforms/grass-mid.png',
     './assets/platforms/grass-single.png',
     './assets/other/bolt_sprite.png',
+    './assets/other/magic-missile.png'
   ]);
 
   setInputListeners();
 
   resources.onReady( () => {
     const background = document.getElementById('background-canvas')
-    background.width = 1200;
-    background.height = 900;
+    background.width = 900;
+    background.height = 675;
     const bgCtx = background.getContext("2d");
     const bgPattern = bgCtx.createPattern(resources.get('./assets/backgrounds/world-background.png'), 'repeat');
     bgCtx.fillStyle = bgPattern;
@@ -33,8 +34,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const gameCanvas = document.getElementById('game-canvas')
     const gameCtx = gameCanvas.getContext("2d");
-    gameCanvas.width = 1200;
-    gameCanvas.height = 900;
+    gameCanvas.width = 900;
+    gameCanvas.height = 675;
 
     const ssGame = new SavingSister(gameCtx, gameCanvas, resources);
 
